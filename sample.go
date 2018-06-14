fc713cc008447a2a515bb4d005fffc603f81c5da
kurosawa <kurosawa.yk@gmail.com>
2018-06-14 15:51:19 +0000

    first commit

--- sample.go
+++ sample.go
@@ -7,6 +7,7 @@ package main
 import "C"
 import "unsafe"
 import "fmt"
+import "runtime"
 
 type Affine struct {
 	dw float64
@@ -20,30 +21,39 @@ type Affine struct {
 // 	backward
 //}
 
-func cUDA_CHECK (C.cudaError_t error) () {
-	fmt.Printf("%s",C.cudaGetErrorString(error))
+func CUDA_CHECK (error C.cudaError_t) {
+	if error != 0 {
+		er_message := C.GoString(C.cudaGetErrorString(error))
+		fmt.Print("ERR:",er_message,"  ")
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Println(file,line)
+	}
 }
+
+func CUDNN_CHECK (error C.cudnnStatus_t) {
+	if error != C.CUDNN_STATUS_SUCCESS {
+		er_message := C.GoString(C.cudnnGetErrorString(error))
+		fmt.Print("ERR:",er_message,"  ")
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Println(file,line)
+	}
+}
+
 
 func Host2GPU (input []float64) (unsafe.Pointer) {
 	var in_data_dev unsafe.Pointer
 	size := C.size_t(len(input))
-	in_unsafe := unsafe.Pointer(in_data_dev)
-	C.cudaMalloc(&in_unsafe, size)
-	C.cudaMemcpy(unsafe.Pointer(in_data_dev), unsafe.Pointer(&input[0]), size ,C.cudaMemcpyHostToDevice)
+	C.cudaMalloc(&in_data_dev, size)
+	C.cudaMemcpy(in_data_dev, unsafe.Pointer(&input[0]), size ,C.cudaMemcpyHostToDevice)
 	return in_data_dev
 }
 
 func GPU2Host (dev_data unsafe.Pointer, hostdata []float64) () {
-	//var in_data_dev *float64
-	//fmt.Print(len(input))
-	//C.cudaMalloc(&in_unsafe, size)
-	C.cudaMemcpy(unsafe.Pointer(&hostdata[0]),
+	CUDA_CHECK(C.cudaMemcpy(unsafe.Pointer(&hostdata[0]),
 	 	dev_data,
-	 	C.size_t(len(hostdata)), C.cudaMemcpyDeviceToHost)
-	C.cudaFree(dev_data)
+	 	C.size_t(len(hostdata)), C.cudaMemcpyDeviceToHost))
+	CUDA_CHECK(C.cudaFree(dev_data))
 }
-
-
 
 //forward (affine Affine, input float64) (float64) {
 //
@@ -57,54 +67,39 @@ func main() {
 	var c C.int = 2
 	var h C.int = 3
 	var w C.int = 3
-	C.cudaSetDevice(0)
-	//var in_data_dev *float64
-	//	var out_data_dev *float64
-	//in_unsafe := unsafe.Pointer(in_data_dev)
-	//out_unsafe := unsafe.Pointer(out_data_dev)
-	//in_data := [3]float64{1,1,1}
+	CUDA_CHECK(C.cudaSetDevice(0))
 	in_data := []float64{
 		0, 1, 0 , 0, 1, 0 , 0, 1, 0,
 		2, 1, 1 , 1, 1, 1 , 1, 1, 1,
 	}
 	out_data := []float64{
-		0, 1, 0 , 0, 1, 0 , 0, 1, 0,
+		2, 1, 1 , 1, 1, 1 , 1, 1, 1,
 		2, 1, 1 , 1, 1, 1 , 1, 1, 1,
 	}
-	//fmt.Print(cap(in_data))
-	//size_in := C.size_t(unsafe.Sizeof(in_data))
-	//C.cudaMalloc(&in_unsafe, size_in)
-	//C.cudaMalloc(&out_unsafe, size_in)
-	//fmt.Print(out_data)
-	//C.cudaMemcpy(unsafe.Pointer(in_unsafe), unsafe.Pointer(&in_data), size_in,C.cudaMemcpyHostToDevice)
-	//C.cudaMemcpy(unsafe.Pointer(out_unsafe), unsafe.Pointer(&out_data), size_in,C.cudaMemcpyHostToDevice)
-	//fmt.Print(&in_data,"\n")
+	fmt.Println("in_data",in_data)
+	fmt.Println("out_data",out_data)
 	in_unsafe := unsafe.Pointer(Host2GPU(in_data))
 	out_unsafe := unsafe.Pointer(Host2GPU(out_data))
 	// cudnn
 	var cudnnHandle C.cudnnHandle_t
 	var srcTensorDesc, dstTensorDesc C.cudnnTensorDescriptor_t
-	C.cudnnCreate(&cudnnHandle)
-	C.cudnnCreateTensorDescriptor(&srcTensorDesc)
-	C.cudnnCreateTensorDescriptor(&dstTensorDesc)
-
-	C.cudnnSetTensor4dDescriptor(srcTensorDesc, C.CUDNN_TENSOR_NCHW, C.CUDNN_DATA_FLOAT, n, c, h, w)
-	C.cudnnSetTensor4dDescriptor(dstTensorDesc, C.CUDNN_TENSOR_NCHW, C.CUDNN_DATA_FLOAT, n, c, h, w)
-
-	var alpha float64 = 1.0
-	var beta float64 = 3.0
-	C.cudnnAddTensor(cudnnHandle,
+	CUDNN_CHECK(C.cudnnCreate(&cudnnHandle))
+	CUDNN_CHECK(C.cudnnCreateTensorDescriptor(&srcTensorDesc))
+	CUDNN_CHECK(C.cudnnCreateTensorDescriptor(&dstTensorDesc))
+	CUDNN_CHECK(C.cudnnSetTensor4dDescriptor(srcTensorDesc, C.CUDNN_TENSOR_NCHW,
+		C.CUDNN_DATA_FLOAT, n, c, h, w))
+	CUDNN_CHECK(C.cudnnSetTensor4dDescriptor(dstTensorDesc, C.CUDNN_TENSOR_NCHW,
+		C.CUDNN_DATA_FLOAT, n, c, h, w))
+	var alpha float64 = 100000.0
+	var beta float64 = 10000000.0
+	CUDNN_CHECK(C.cudnnAddTensor(cudnnHandle,
 	    unsafe.Pointer(&alpha),
 	    srcTensorDesc,
 	    in_unsafe,
 	    unsafe.Pointer(&beta),
 	    dstTensorDesc,
-	    out_unsafe)
-
-	//C.cudaMemcpy(unsafe.Pointer(&out_data), unsafe.Pointer(out_unsafe), size_in,C.cudaMemcpyDeviceToHost)
-	fmt.Print(out_data,"\n")
+	    out_unsafe))
 	GPU2Host(out_unsafe, out_data)
-	fmt.Print(out_data,"\n")
-	C.cudaFree(in_unsafe)
-	//C.cudaFree(out_unsafe)
+	fmt.Println("out_data",out_data)
+	CUDA_CHECK(C.cudaFree(in_unsafe))
 }
