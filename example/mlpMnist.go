31f56404fc98e00061e1d62188d05381ea3c6d92
kurosawa <kurosawa.yk@gmail.com>
2018-07-20 00:34:24 +0900

    change to keras mnist param

--- example/mlpMnist.go
+++ example/mlpMnist.go
@@ -8,33 +8,43 @@ import (
 )
 
 func main() {
-	batchSize := 100
-	inputSize := 784
-	hiddenSize := 50
-	outputSize := 10
-	learningRate := 0.01
-	iterationNum := 2000
-
-	w0 := gmat.HeNorm2D(inputSize, hiddenSize)
-	b0 := gmat.Make(batchSize, hiddenSize)
-	w1 := gmat.HeNorm2D(hiddenSize, outputSize)
-	b1 := gmat.Make(batchSize, outputSize)
-
-	layer := []gdeep.LayerInterface{}
-	dense1 := &gdeep.Dense{W: w0, B: b0}
-	relu1 := &gdeep.Relu{}
-	dense2 := &gdeep.Dense{W: w1, B: b1}
-	softmaxWithLoss := gdeep.SoftmaxWithLoss{}
-	layer = append(layer, dense1)
-	layer = append(layer, relu1)
-	layer = append(layer, dense2)
-
-	//sgd := &gdeep.SGD{learningRate}
-	momentum := &gdeep.Momentum{learningRate, 0.9}
 
 	train, test, _ := GoMNIST.Load("./data")
 	trainDataSize := len(train.ImagesFloatNorm)
 	testDataSize := len(test.ImagesFloatNorm)
+
+	batchSize := 128
+	inputSize := 784
+	hiddenSize := 512
+	outputSize := 10
+	learningRate := 0.01
+	epochNum := 20
+	iterationNum := trainDataSize * epochNum / batchSize
+
+	w0 := gmat.HeNorm2D(inputSize, hiddenSize)
+	b0 := gmat.Make(batchSize, hiddenSize)
+	w1 := gmat.HeNorm2D(hiddenSize, hiddenSize)
+	b1 := gmat.Make(batchSize, hiddenSize)
+	w2 := gmat.HeNorm2D(hiddenSize, outputSize)
+	b2 := gmat.Make(batchSize, outputSize)
+
+	layer := []gdeep.LayerInterface{}
+	dense1 := &gdeep.Dense{W: w0, B: b0}
+	relu1 := &gdeep.Relu{}
+	dropout1 := &gdeep.Dropout{Train: true, Ratio: 0.2}
+	dense2 := &gdeep.Dense{W: w1, B: b1}
+	relu2 := &gdeep.Relu{}
+	dropout2 := &gdeep.Dropout{Train: true, Ratio: 0.2}
+	dense3 := &gdeep.Dense{W: w2, B: b2}
+	softmaxWithLoss := gdeep.SoftmaxWithLoss{}
+	layer = append(layer, dense1, relu1)
+	layer = append(layer, dropout1)
+	layer = append(layer, dense2, relu2)
+	layer = append(layer, dropout2)
+	layer = append(layer, dense3)
+	//sgd := &gdeep.SGD{learningRate}
+	momentum := &gdeep.Momentum{learningRate, 0.9}
+
 	iter := 0
 	for i := 0; i < iterationNum; i++ {
 		if (i+2)*batchSize > trainDataSize {
@@ -60,6 +70,8 @@ func main() {
 	// test accuracy
 	accuracy := 0.0
 	iterBach := testDataSize / batchSize
+	dropout1.Train = false
+	dropout2.Train = false
 	for i := 0; i < iterBach; i++ {
 		x := test.ImagesFloatNorm[:][i*batchSize : (i+1)*batchSize]
 		t := test.LabelsOneHot[:][i*batchSize : (i+1)*batchSize]
